3b34c8536b863ce0545385556c3df44365a8e752
Chen Zhou <czwork@outlook.com>
2015-10-25 22:32:56 +0800

    add racket solution

---
+++ problem10/solution.rkt
@@ -0,0 +1,13 @@
+#|
+ | The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ |
+ | Find the sum of all the primes below two million.
+ |#
+#lang racket
+
+(require math/number-theory)
+
+(define LIMIT 2000001)
+
+(foldl + 0 (filter prime? (range 0 LIMIT)))
+#| 142913828922 |#
