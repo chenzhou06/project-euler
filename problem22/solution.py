003e2a2b1475d45be6dd7d3262e891d3400a844d
Chen Zhou <czwork@outlook.com>
2015-11-09 11:58:44 +0800

    add python solution

--- problem22/solution.py
+++ problem22/solution.py
@@ -1,4 +1,31 @@
 #!/bin/env python3
 
+
+   # Using [1]names.txt, a 46K text file containing over five-thousand first
+   # names, begin by sorting it into alphabetical order. Then working out the
+   # alphabetical value for each name, multiply this value by its alphabetical
+   # position in the list to obtain a name score.
+
+   # For example, when the list is sorted into alphabetical order, COLIN, which
+   # is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So,
+   # COLIN would obtain a score of 938 × 53 = 49714.
+
+   # What is the total of all the name scores in the
+
+from string import ascii_uppercase as letters
+
+def letter_score(letter):
+    return letters.index(letter) + 1
+
+def name_score(name):
+    return sum([letter_score(l) for l in name])
+
 with open("names.txt", "r") as f:
-    print(f.read().split(","))
+    bigstring = f.read().split(",")
+    names = sorted([s.strip("\"") for s in bigstring])
+
+if __name__ == "__main__":
+    n = 0
+    for i, name in enumerate(names):
+        n += name_score(name) * (i+1)
+    print(n)
